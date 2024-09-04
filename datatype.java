44b2881b62af86209d59737b9f78c65c06fe55ac
muthukumar0406 <muthukumarbsc0406@gmail.com>
2024-09-04 11:51:46 +0530

    added

---
+++ datatype.java
@@ -0,0 +1,13 @@
+
+
+public class datatype {
+    public static void main(String[]args){
+        int a=10;
+        double b=25.00;
+        String c="kumar";
+
+        System.out.println(a ,b ,c);
+        
+    }
+    
+}

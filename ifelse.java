0684450d7115e6b3633b68c12d7024b197a96ebc
muthukumar0406 <muthukumarbsc0406@gmail.com>
2024-09-04 17:00:22 +0530

    added

---
+++ ifelse.java
@@ -0,0 +1,14 @@
+public class ifelse {
+    public static void main(String[]args){
+    int a =100;
+    int b = 10 ;
+    if (a<b) {
+        System.out.println("a is greathan b");
+    }
+    else{
+        System.out.println("a is smaller than b");
+       
+    }
+    
+}
+}

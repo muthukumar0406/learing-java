2e5951e80871c8f218858647be0889d3abbcf88a
muthukumar0406 <muthukumarbsc0406@gmail.com>
2024-09-05 21:13:33 +0530

    added

---
+++ breakcontinue.java
@@ -0,0 +1,23 @@
+public class breakcontinue {
+   public static void main(String[] args) {
+    int i=0;
+    while (i<10) {
+        System.out.println(i);
+        i++;
+    if(i==4){
+        break;
+    }
+}
+    int j=0;
+    while (j<=10) {
+    System.out.println(j);
+    j++;
+    
+    if(j==4){
+        continue;
+    }
+        
+    }
+   
+   }    
+}

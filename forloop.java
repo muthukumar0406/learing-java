88764778e47604a5c7c2f5d23bb50a9c150d39f9
muthukumar0406 <muthukumarbsc0406@gmail.com>
2024-09-05 18:36:05 +0530

    added

---
+++ forloop.java
@@ -0,0 +1,11 @@
+//write a program to print the number from 1 to 10 and print number fron 10 to 1
+
+public class forloop {
+    public static void main(String [] args){
+        for(int i=1;i<=10;i++){
+                  
+            System.out.println(i);
+       
+        }
+    }
+}

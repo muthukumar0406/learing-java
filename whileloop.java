07a51e54f1d203aadeaacddf4de056bb84ae56d9
muthukumar0406 <muthukumarbsc0406@gmail.com>
2024-09-05 18:44:26 +0530

    added

---
+++ whileloop.java
@@ -0,0 +1,9 @@
+public class whileloop {
+    public static void main (String[]args){
+        int countdown=3;
+        while(countdown>0){
+            System.out.println(countdown--);
+        }
+        System.out.println("happy birthday!");
+    }
+}

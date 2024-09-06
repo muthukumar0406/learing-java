8fce81de56e7f551c5c4ed8b167f362caced4348
muthukumar0406 <muthukumarbsc0406@gmail.com>
2024-09-06 14:46:18 +0530

    added

---
+++ array.java
@@ -0,0 +1,10 @@
+public class array {
+    public static void main(String[]args)
+    {
+        String []cars={"bmw","bens","supra","audi"};
+        int [] num={1,2,3,4,5,6};
+        System.out.println(cars[0]);
+        System.out.println(num[4]);
+    }
+    
+}

00e6708248eb2d2d1924b9e8bc2dfe2523dd5db8
muthukumar0406 <muthukumarbsc0406@gmail.com>
2024-09-10 20:27:46 +0530

    added

---
+++ method.java
@@ -0,0 +1,19 @@
+public class method {
+    static void mymethod()
+    {
+        System.out.print("hello ");
+    }
+    static void mymethod1()
+    {
+        System.err.print("i am a ");
+    }
+    static void mymethod2()
+    {
+        System.out.println("java developer");
+    }
+    public static void main(String[] args) {
+        mymethod();
+        mymethod1();
+        mymethod2();
+    }
+}

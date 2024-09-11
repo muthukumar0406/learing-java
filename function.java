5ca1830098cc8b650e4419e5d41abfa561e0fcf7
muthukumar0406 <muthukumarbsc0406@gmail.com>
2024-09-11 13:34:50 +0530

    added

---
+++ function.java
@@ -0,0 +1,22 @@
+public class function {
+    void bmw()
+    {
+        System.out.println("The BMW M5");
+    }
+    void audi()
+    {
+        System.out.println("The Audi RS series ");
+    }
+    void hondo()
+    {
+        System.out.println("The Honda Civic ");
+    }
+
+    public static void main(String[]args){
+        function cars=new function();
+        cars.bmw();
+        cars.audi();
+        cars.hondo();
+    }
+    
+}

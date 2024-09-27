a24a038756ed1dd4981431f183d789c48f35e4c0
muthukumar0406 <muthukumarbsc0406@gmail.com>
2024-09-27 07:46:22 +0530

    added

---
+++ constructors.java
@@ -0,0 +1,22 @@
+public class constructors {
+    int a;
+    String b;
+    constructors()
+    {
+        System.out.println("hello java");
+    }
+    public static void main(String []args){
+        constructors obj=new constructors();
+        System.out.println(obj.a);
+        System.out.println(obj.b);
+    }
+
+
+
+
+
+
+
+    
+    
+}

37cb807f8516896a1893b22163bcd9bb4a107b41
muthukumar0406 <muthukumarbsc0406@gmail.com>
2024-09-26 21:05:16 -0700

    Add files via upload

---
+++ enchanced_for_loop.java
@@ -0,0 +1,16 @@
+public class enchanced_for_loop {
+    public static void main(String[]args){
+        int num[]={6,7,8,9};
+        int num1[]={5,6,7,8};
+        for(int i=0;i<=4;i++)
+        {
+            System.out.println(num[i]);
+        
+        for(int var:num1)
+        {
+            System.out.println(var);
+        }
+    }
+    }
+    
+}

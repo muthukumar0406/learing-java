055a751c6b3690bb0f6f2bc0f6fcf2e73fba180e
muthukumar0406 <muthukumarbsc0406@gmail.com>
2024-10-02 07:43:30 +0530

    added

---
+++ inheritance.java
@@ -0,0 +1,15 @@
+class parent{
+    int money=1000;
+
+}
+class child extends parent{
+
+}
+public class inheritance {
+public static void main(String[] args) {
+    child obj1= new child();
+
+    System.out.println(obj1.money);
+}
+    
+}

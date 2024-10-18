a5f15bed5efd13807486771006cb818a9e8dd52a
muthukumar0406 <muthukumarbsc0406@gmail.com>
2024-10-18 21:12:49 +0530

    added

---
+++ access.java
@@ -0,0 +1,15 @@
+
+     class teacher{
+        private String t_name="kumar";
+        void display(){
+            System.out.println(t_name);
+        }
+    }
+    public class access {
+    public static void main(String[] args) {
+        teacher obj=new teacher();
+        obj.display();
+       
+    }
+    
+}
